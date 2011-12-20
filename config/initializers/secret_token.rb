0ff9b56b5c689229067a4c594d12d039b4632f2d
Rafael Rosa Fu <rafaelrosafu@gmail.com>
2011-12-19 23:37:04 -0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Livra::Application.config.secret_token = 'ce4a26d54396637aa555b9e8cacd2f3e8f78b883d5d4a9d3fcfc4b7f568fef3ab1f6f84de9c754d8ec5a9e07b0282708ee64e19cfa172e999f2663b4bd95d329'
