e3a869f8fae07570e94e97a6c91aa1bde450057f
Rafael Rosa Fu <rafaelrosafu@gmail.com>
2011-12-28 12:36:53 -0200

    Fixed annoying Postgres bug

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,10 +1,10 @@
 class Answer < ActiveRecord::Base
   scope :confirmed, where(:confirmed => true)
   scope :unconfirmed, where(:confirmed => false)
-  scope :by_user, order(:user)
+  scope :by_user, order('answers.user')
   scope :by_question_number, order(:question_number)
 
   def self.confirmed_users
-    Answer.select('DISTINCT user').confirmed.count
+    Answer.select('DISTINCT answers.user').confirmed.count
   end
 end
